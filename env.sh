f3b6badcbf73a73420b97fdf7404c8bf63d99a14
Boyang Li <boyang@localhost.localdomain>
2020-08-01 03:26:29 -0700

    support gcc10.2

--- env.sh
+++ env.sh
@@ -11,3 +11,8 @@ if [ -z ${FCNC_DIR+x} ] ; then
 fi
 source $FCNC_DIR/PlotTools/env.sh
 source $FCNC_DIR/FCNCAnalysis/env.sh
+
+alias tthcd='cd $ttH_fakes_DIR'
+alias tthmake='cd $ttH_fakes_DIR/build; make; cd -'
+alias plotcd='cd $PLOT_LIB_DIR'
+alias plotmake='cd $PLOT_LIB_DIR/build; make; cd -'
--- setup.sh
+++ setup.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
+
+git submodule init
+
+git submodule update
+
 source env.sh
 
+
+for word in `root-config --cflags`
+do
+	if [[ "$word" =~ "-std=" ]] ; then
+		CXX_STANDARD=`echo $word | tr -dc '0-9'`
+		break;
+	fi
+done
+
+echo "-- Detected root CXX standard:" $CXX_STANDARD
+
 cd PlotTools
-mkdir build
+mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_CXX_STANDARD=$CXX_STANDARD
 make
 
 cd ../../FCNCAnalysis
-mkdir build
+mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_CXX_STANDARD=$CXX_STANDARD
 make install
 
-cd ../..
+cd ../../TRExFitter
+
+git submodule init
+
+git submodule update
+
+. setup.sh
+
+cd ..
